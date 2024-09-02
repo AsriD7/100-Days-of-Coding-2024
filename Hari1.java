78c1bf7351683c34c2e1c2bf5551291aa6a56a00
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-09-02 13:13:52 +0800

    Hari1.java
    Membuat Project dan class

---
+++ Hari1.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class main{
+  public static void main(String[]args ){
+    System.out.println("Nama : Asri");
+    System.out.println("NIM : D0223532");
+    System.out.println("Alamat : Petoosang");
+    System.out.println("motto hidup : Glory-glory man united");
+  }
+
+}
