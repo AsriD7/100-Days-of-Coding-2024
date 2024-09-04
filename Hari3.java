f235e8ad8d124c67858456b06f1ea9fdcffd041b
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-09-04 18:37:15 +0800

    System.out.println();

---
+++ Hari3.java
@@ -0,0 +1,13 @@
+package Mulai;
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 7;
+        if(a!=b){
+            System.out.println("Decul");
+        }else{
+            System.out.println("demik");
+        }
+    }
+}
