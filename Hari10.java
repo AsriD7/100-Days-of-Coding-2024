cfc1bc0c976de11b2e2e6a6bdaa3712e0b86d6bf
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-09-11 05:29:20 +0800

    Byte

---
+++ Hari10.java
@@ -0,0 +1,11 @@
+package mulai;
+
+public class Day10 {
+    
+    public static void main(String[] args) {
+        byte a = -128, b = 127;
+        
+        System.out.printf("Bilangan bulat dari "+a+" Sampai "+b);
+    }
+    
+}
