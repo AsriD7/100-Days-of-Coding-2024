3cb7a4f0c089304db796fd4403b3c929e623698c
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-09-12 00:17:45 +0800

    Short

---
+++ Hari11.java
@@ -0,0 +1,10 @@
+package mulai;
+
+public class Day11 {
+    
+    public static void main(String[] args) {
+        short a = -32768, b = 32767;
+        System.out.println("Bilangan bulat dari "+a+" Sampai "+b);
+    }
+    
+}
