1d7923908ec90c2e3f1a71cbd9eeb0e307fbb2c2
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-09-13 00:08:44 +0800

    int

---
+++ Hari12.java
@@ -0,0 +1,9 @@
+package mulai;
+
+public class Day12 {
+    public static void main(String[]args){
+        int a = -2147483648, b = 2147483647;
+        System.out.println("Bilangan bulat dari "+a+" Hingga "+b);
+    }
+    
+}
