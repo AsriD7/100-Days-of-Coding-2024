75abcef53ec366505c3ed9d2c7dd1cfaf055ae06
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-09-19 22:57:07 +0800

    Boolean

---
+++ Hari18.java
@@ -0,0 +1,10 @@
+package mulai;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("Kamu itu hanya badut : "+a);
+    }
+}
