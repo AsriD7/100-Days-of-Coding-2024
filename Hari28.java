62d65021a25434efc19ec4eacff11f4415f0ebe5
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-09-29 23:35:14 +0800

    Reference Boolean

--- Hari28.java
+++ Hari28.java
@@ -4,6 +4,6 @@ public class Day28 {
   public static void main(String[]args){
     Boolean benar = true;
     Boolean salah = false;
-    System.out.println(salah);
+    System.out.println(benar);
   }
 }
