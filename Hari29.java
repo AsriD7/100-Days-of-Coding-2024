e5015b3550a9bd55509a2cfa5d2b78edcca99f26
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-09-30 23:47:45 +0800

    Reference BigInteger

---
+++ Hari29.java
@@ -0,0 +1,8 @@
+package mulai;
+import java.math.BigInteger;
+public class Day29 {
+  public static void main(String[]args){
+    BigInteger a = new BigInteger("1000000000");
+    System.out.println(a);
+  }
+}
