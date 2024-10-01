0684ca5286192c425b05d07d2c513ee5cb33591f
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-01 23:54:28 +0800

    Reference BigDecimal

---
+++ Hari30.java
@@ -0,0 +1,8 @@
+package mulai;
+import java.math.BigDecimal;
+public class Day30{
+  public static void main(String[]args){
+    BigDecimal a = new BigDecimal("1.1000000000");
+    System.out.println(a);
+  }
+}
