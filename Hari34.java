dc05972ba343c7819569971c3828859a0f8bea13
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-05 18:58:34 +0800

    Input keyboard menggunakan Scanner

---
+++ Hari34.java
@@ -0,0 +1,9 @@
+package mulai;
+import java.util.Scanner;
+public class Day34{
+  public static void main(String[]args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Masukkan nama : ");
+    String nama = input.nextLine();
+  }
+}
