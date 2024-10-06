9091751d98d8a54bf08661be1d2e5a6c58fcdd2c
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-06 22:00:54 +0800

    Operator Aritmatika : Penjumlahan & Pengurangan

---
+++ Hari35.java
@@ -0,0 +1,11 @@
+package mulai;
+
+public class Day35{
+  public static void main(String[]args){
+    int a = 3;
+    int b = 2;
+    int penjumlahan = a + b;
+    int pengurangan = a - b;
+    System.out.println(penjumlahan+" "+pengurangan);
+  }
+}
