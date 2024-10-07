6d8fca277908ace978d1e9f87d13f16054ab3bf1
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-07 22:36:28 +0800

    Operator Aritmatika : Perkalian, Pembagian serta Modulo

---
+++ Hari36.java
@@ -0,0 +1,12 @@
+package mulai;
+
+public class Day36{
+  public static void main(String[] args) {
+    int a = 1;
+    int b = 2;       
+    int pkl = a * b;
+    int pbn = a / b;  
+    int mdl = a % b;
+    System.out.println(pkl+" "+pbn+" "+mdl);
+  }
+}
