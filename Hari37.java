88298045917db497b45f39bd5cfbfe89e8d94e6a
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-08 23:37:38 +0800

    Create Hari37.java

---
+++ Hari37.java
@@ -0,0 +1,11 @@
+package mulai;
+public class Main {
+    public static void main(String[] args) {
+        int a = 5; 
+        System.out.println("Nilai a: " + a);
+        a += 3;  
+        System.out.println("Nilai a setelah penambahan: " + a);
+        a -= 2;  
+        System.out.println("Nilai a setelah pengurangan: " + a);
+    }
+}
