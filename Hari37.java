52d3d19089218d0655447fd7d308df71d62690bb
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-09 23:16:37 +0800

    Operator Penugasan : Pengisian Nilai, Pengisian dan  Penambahan, Pengisian dan Pengurangan

--- Hari37.java
+++ Hari37.java
@@ -2,7 +2,7 @@ package mulai;
 public class Main {
     public static void main(String[] args) {
         int a = 5; 
-        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai a: " +a);
         a += 3;  
         System.out.println("Nilai a setelah penambahan: " + a);
         a -= 2;  
