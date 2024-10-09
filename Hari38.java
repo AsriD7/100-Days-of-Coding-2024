6eb7e587d96fb1e2597d8c19f64e30bbaa76b6d3
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-09 23:13:32 +0800

    Operator Penugasan : Pengisian dan Perkalian, Pengisian dan  Pembagian

---
+++ Hari38.java
@@ -0,0 +1,12 @@
+package ManUnited;
+
+public class Day38{
+  public static void main(String[]args){   
+    int a = 5;        
+    int b = 6;
+    a *= 3;  
+    System.out.println(a); 
+    b /= 6;   
+    System.out.println(b);  
+  }
+}
