960675f01a01c4b07e52b467d10c939770331689
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-11 23:45:42 +0800

    Operator Perbandingan : Lebih besar dan lebih kecil

---
+++ Hari40.java
@@ -0,0 +1,12 @@
+package mulai;
+
+public class Day40{      
+  public static void main(String[] args) {       
+    int a = 2;     
+    int b = 3;    
+    boolean lb = a > b;    
+    boolean lk = a < b;   
+    System.out.println("pakah lebih besar ? "+lb);  
+    System.out.println("pakah lebih kecil ? "+lk);     
+    }
+}
