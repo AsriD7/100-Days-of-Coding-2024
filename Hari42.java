7a862596ccedb120091832d4d02f9a85dbc62382
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-13 22:49:04 +0800

    Operator Perbandingan : Sama dengan dan tidak sama dengan

---
+++ Hari42.java
@@ -0,0 +1,10 @@
+package mulai;
+
+public class Day42{
+  public static void main(String[]args){
+    boolean sama_dengan = 5 == 6;
+    boolean tidak_samaDengan = 5 != 6;
+    System.out.println(sama_dengan);
+    System.out.pribtln(tidak_samaDengan);
+  }
+}
