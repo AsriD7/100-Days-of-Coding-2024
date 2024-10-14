4dff69ccc249373b87601af1db92240b29bf1ee4
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-14 23:54:13 +0800

    Operator Perbandingan : Lebih besar sama dengan dan lebih  kecil sama dengan

---
+++ Hari43.java
@@ -0,0 +1,10 @@
+package mulai;
+
+public class Day43 {
+    public static void main(String[] args) {
+        boolean lbsm = 5 >= 6;
+        boolean lksm = 5 <= 6;
+        System.out.println(lbsm);
+        System.out.println(lksm);
+    }
+}
