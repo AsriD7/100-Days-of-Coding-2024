44d5e713c52a730e0ae11db678590916017dee8a
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-16 05:56:37 +0800

    Or

---
+++ Hari46.java
@@ -0,0 +1,10 @@
+package B2;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean lbsm = 5 >= 6;
+        boolean lksm = 5 <= 6;
+        boolean or = lbsm || lksm;
+        System.out.println(or);
+    }
+}
