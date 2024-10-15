41f6fdb7c6f3bc04e10065ca8cc8dd0767e69830
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-16 05:54:28 +0800

    And

---
+++ Hari44.java
@@ -0,0 +1,10 @@
+package mulai;
+
+public class Day43 {
+    public static void main(String[] args) {
+        boolean lbsm = 5 >= 6;
+        boolean lksm = 5 <= 6;
+        boolean and = lbsm && lksm;
+        System.out.println(and);
+    }
+}
