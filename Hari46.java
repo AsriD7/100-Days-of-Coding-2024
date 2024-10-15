95dbbfaa11e7cafafcc4fa5b88b5a8365fad6d73
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-16 06:09:52 +0800

    negasi

---
+++ Hari46.java
@@ -0,0 +1,9 @@
+package mulai;
+
+public class Day46 {
+    public static void main(String[] args) {
+        boolean konspirasi = false;
+        boolean ternyata = ! konspirasi;
+        System.out.println(ternyata);
+    }
+}
