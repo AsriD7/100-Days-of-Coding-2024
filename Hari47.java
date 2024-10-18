b9a53f574c7269634d15d3c662fd01d300ab0b50
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-18 23:19:58 +0800

    Percabangan if

---
+++ Hari47.java
@@ -0,0 +1,10 @@
+package mulai;
+
+public class Day47 {
+    public static void main(String[] args) {
+        boolean konspirasi = false;
+        if(konspirasi==true){
+            System.out.println("Kebenaran");
+        }
+    }
+}
