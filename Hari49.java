9472ae17ab6749f55b336ee755d1a7a402ff3176
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-20 22:58:17 +0800

    Percabangan if, else if dan else

---
+++ Hari49.java
@@ -0,0 +1,14 @@
+package mulai;
+
+public class Day49 {
+    public static void main(String[] args) {
+        boolean cek = false;
+        if(cek==true){
+            System.out.println("sze");
+        }else if(cek==false){
+            System.out.println("yb");
+        }else{
+            System.out.println("jindan");
+        }
+    }
+}
