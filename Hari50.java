6771a612dfd6bbd9a0e78a15cae608124ca991c1
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-21 19:45:52 +0800

    Percabangan if else

---
+++ Hari50.java
@@ -0,0 +1,12 @@
+package mulai;
+
+public class Day50{
+  public static void main(String[]args){
+    boolean cek = true;
+    if(cek==false){
+      System.out.println("mm");
+    }else{
+      System.out.println("exp");
+    }
+  }
+}
