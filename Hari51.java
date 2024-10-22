475420c09b6e6e0efd9cac8b66ac3b553da43548
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-22 23:09:52 +0800

    Percabangan : Switch Case

---
+++ Hari51.java
@@ -0,0 +1,18 @@
+package mulai;
+
+public class Day51 {
+    public static void main(String[] args) {
+        int pilihan = 1;
+        switch (pilihan){
+            case 1:
+            System.out.println("rose");
+            break;
+            case 2:
+            System.out.println("lisa");
+            break;
+            default:
+            System.out.println("beyonce");
+        }
+  
+    }
+}
