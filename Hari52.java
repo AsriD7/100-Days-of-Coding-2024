9ea4b8e05491901274be27a68d950f68f4bbbbd3
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-23 22:43:00 +0800

    Operator ternary

---
+++ Hari52.java
@@ -0,0 +1,10 @@
+package mulai;
+
+public class Main {
+    public static void main(String[] args) {
+        int nilai = 49;
+        String ternary = nilai > 50 ? "baik":"error";
+        System.out.println(ternary);
+  
+    }
+}
