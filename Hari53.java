0dfc8257987bb6a864092bb53847e13da2e5d3b1
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-24 23:27:26 +0800

    Looping for

---
+++ Hari53.java
@@ -0,0 +1,9 @@
+package mulai;
+
+public class Day53 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+           System.out.println("Asri"); 
+        }
+    }
+}
