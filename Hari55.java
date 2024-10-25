fa4853db2ed6e2da2c6fa4e771350311d668e394
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-25 23:22:26 +0800

    Looping : for Increment

---
+++ Hari55.java
@@ -0,0 +1,10 @@
+package mulai;
+
+public class Day55 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 3; i++){
+           System.out.print(i+" : Isc. "); 
+        }
+        
+    }
+}
