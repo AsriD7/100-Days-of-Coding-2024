fc43115634dd373340d0178ea0c00380bdbeb73c
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-28 23:30:15 +0800

    Looping while

---
+++ Hari57.java
@@ -0,0 +1,12 @@
+package mulai;
+
+public class Day57 {
+    public static void main(String[] args) {
+        int nilai = 1;
+        int target = 10;
+        while (nilai<=target){
+            System.out.println(nilai);
+            nilai++;
+        }
+    }
+}
