cb74d940e37234d1ebe63e1b8c983a043f46588c
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-29 23:54:34 +0800

    Looping while decrement

---
+++ Hari58.java
@@ -0,0 +1,12 @@
+package mulai;
+
+public class Day58 {
+    public static void main(String[] args) {
+        int nilai = 10;
+        int target = 1;
+        while (nilai>=target){
+            System.out.println(nilai);
+            nilai--;
+        }
+    }
+}
