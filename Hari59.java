97655e54fc641d491014b812ae7cece481b9704c
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-10-30 22:33:17 +0800

    Looping for each

---
+++ Hari59.java
@@ -0,0 +1,11 @@
+package mulai;
+
+public class Day59 {
+    public static void main(String[] args) {
+        int [] a = {4, 6, 9, 2};
+        for(int b : a){
+            System.out.print(b+" ");
+        }
+        
+    }
+}
