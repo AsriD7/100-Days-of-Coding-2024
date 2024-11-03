547eed722b9fc45e2d1442d7602d9e9011beea97
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-11-03 23:48:35 +0800

    Continue

---
+++ Hari63.java
@@ -0,0 +1,14 @@
+package mulai;
+
+public class Day63 {
+    
+    public static void main(String[] args) {
+        
+        for(int i = 1; i <= 5; i++ ){
+            if(i==2){
+                continue;
+            }
+         System.out.print(i+" ");   
+        }
+    }
+}
