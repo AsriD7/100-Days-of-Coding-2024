d602ca141a525f82081b19a5be7d533f92897f6c
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-11-07 23:17:42 +0800

    Method void

---
+++ Hari67.java
@@ -0,0 +1,11 @@
+package mulai;
+
+public class Main {
+    public void nama(){
+        System.out.println("Asri");
+    }
+    public static void main(String[] args) {
+        Main contoh = new Main();
+        contoh.nama();
+    }
+}
