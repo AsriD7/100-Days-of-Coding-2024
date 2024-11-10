ed1a5549c3a6fca2ee904bdd56c2523b38058d61
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-11-10 23:08:29 +0800

    Method Parameter

---
+++ Hari70.java
@@ -0,0 +1,11 @@
+package mulai;
+
+public class Main {
+    public void Nama(String nama) {
+        System.out.println("Nama: "+ nama);
+    }
+    public static void main(String[] args) {
+        Main a = new Main();
+        a.Nama("Asri");
+    }
+}
