275c93947f80202c3085f7b55c677d09d8da2cd7
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-11-11 15:51:13 +0800

    Method static

---
+++ Hari71.java
@@ -0,0 +1,11 @@
+package mulai;
+
+public class Main {
+    public static String Nama(String nama) {
+        return nama;
+    }
+    public static void main(String[] args) {
+        String nama = Main.Nama("Asri");
+        System.out.println("Nama : "+nama);
+    }
+}
