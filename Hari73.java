6ca20c704c0b6e2173751c953a684bc89d8e9a21
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-11-13 23:48:58 +0800

    String Method : charAt()

---
+++ Hari73.java
@@ -0,0 +1,10 @@
+package mulai;
+public class Main {
+
+    public static void main(String[] args) {
+        String kata = "Buta";
+        char k0 = kata.charAt(0);
+        System.out.println(k0);
+        
+    }
+}
