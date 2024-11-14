7b3831e72e2e8a7b5e424293450d16ef850f6585
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-11-14 23:15:29 +0800

    String Method : length()

---
+++ Hari74.java
@@ -0,0 +1,9 @@
+package mulai;
+public class Main {
+    public static void main(String[] args) {
+        String kata = "Buta";
+        int panjang = kata.length();
+        System.out.println(panjang);
+        
+    }
+}
