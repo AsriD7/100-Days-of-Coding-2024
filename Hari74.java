7414bc993f93414031422e14f333858ca4a3a0f8
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-11-14 23:25:44 +0800

    String Method : length()

--- Hari74.java
+++ Hari74.java
@@ -2,8 +2,7 @@ package mulai;
 public class Main {
     public static void main(String[] args) {
         String kata = "Buta";
-        int panjang = kata.length();
-        System.out.println(panjang);
+        System.out.println(kata.length());
         
     }
 }
