7afdde9e320c6903b02fa71291e183006b1e924b
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-11-22 15:38:13 +0800

    Method ceil(), floor() dan round()

---
+++ Hari82.java
@@ -0,0 +1,10 @@
+package mulai;
+import java.lang.Math;
+public class Main {
+    public static void main(String[] args) {
+        double num = 2.7;
+        System.out.println(Math.ceil(num));
+        System.out.println(Math.floor(num));
+        System.out.println(Math.round(num));
+    }
+}
