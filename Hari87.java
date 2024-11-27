c32c9b55047ab5fafddf5673525cd46882ea09c5
AsriD7 <147138693+AsriD7@users.noreply.github.com>
2024-11-27 23:06:39 +0800

    Mengubah Nilai Array satu dimensi pada index Tertentu

---
+++ Hari87.java
@@ -0,0 +1,11 @@
+package mulai;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        String [] nama = {"Asri","Mawan","Jun"};
+        nama [2] = "putra";
+        System.out.println(Arrays.toString(nama));
+    }
+}
+
+
